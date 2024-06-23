33ee36ba0c8c325816315f1dd75c2a95b7c0bb74
Linda Paz <Lindacsndelapaz@gmail.com>
2024-06-23 15:02:27 -0300

    add team

---
+++ Equipo.java
@@ -0,0 +1,22 @@
+// Nombre: [Tu Nombre]
+// Apellido: [Tu Apellido]
+// Legajo: [Tu Legajo]
+// Fecha: 23/06/2024
+
+class Equipo {
+    String nombre;
+    int puntos;
+    
+    public Equipo(String nombre, int puntos) {
+        this.nombre = nombre;
+        this.puntos = puntos;
+    }
+
+    @Override
+    public String toString() {
+        return "Equipo{" +
+                "nombre='" + nombre + '\'' +
+                ", puntos=" + puntos +
+                '}';
+    }
+}
